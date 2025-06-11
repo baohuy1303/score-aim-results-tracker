5065f08b443d057ba19ffbba221b45bd91b524de
baohuy1303 <huynhbaohuy130333@gmail.com>
2025-06-12 00:01:47 +0700

    LITTLE THING

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -5,7 +5,7 @@ import '../css/navbar.css'
 function Navbar(){
     return<div className="navbar">
     <Link className="nav-item" to={'/home'}>Home</Link>
-    <Link className="nav-item" to={'/add-subjects'}>Add</Link>
+    <Link className="nav-item" to={'/add-subjects'}>Edit Subject List</Link>
     </div>
 }
 
