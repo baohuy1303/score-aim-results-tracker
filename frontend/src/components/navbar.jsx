0497226892015c854253ab3972c9bd9f3d1cbb52
baohuy1303 <huynhbaohuy130333@gmail.com>
2025-06-14 01:50:50 +0700

    backbutoon

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,9 +1,28 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate, useLocation } from "react-router-dom"
 import '../css/navbar.css'
 
 
 function Navbar(){
+    const location = useLocation()
+    const navigate = useNavigate()
+    let isHome = true
+
+    if(location.pathname === '/home') isHome = true
+    else isHome = false
+
+    const handleBack = () =>{
+        if(window.history.length > 1){
+            if(location.pathname === '/add-subjects'){
+                navigate('/home')
+            }else{
+                navigate(-1)
+            }
+            console.log('Current pathname:', location.pathname);    
+        }
+    }
+
     return<div className="navbar">
+        {!isHome && <button className="nav-item" onClick={handleBack}>Back</button>}
     <Link className="nav-item" to={'/home'}>Home</Link>
     <Link className="nav-item" to={'/add-subjects'}>Edit Subject List</Link>
     </div>
