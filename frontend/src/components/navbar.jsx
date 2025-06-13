0f543b5dffd7e72825caf190978bc88e82d713e9
baohuy1303 <huynhbaohuy130333@gmail.com>
2025-06-14 01:28:48 +0700

    reverting

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,9 +1,25 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate, useLocation} from "react-router-dom"
+import { useState } from "react"
 import '../css/navbar.css'
 
 
 function Navbar(){
+    const location = useLocation()
+    const navigate = useNavigate()
+    let isHome = true
+
+    if(location.pathname === "/home"){
+        isHome = true
+    }else{
+        isHome = false
+    }
+
+    const handleBack = () => {
+        navigate(-1)
+    }
+
     return<div className="navbar">
+    {!isHome && (<Link onClick={handleBack} className="nav-item">Back</Link>)}
     <Link className="nav-item" to={'/home'}>Home</Link>
     <Link className="nav-item" to={'/add-subjects'}>Edit Subject List</Link>
     </div>
