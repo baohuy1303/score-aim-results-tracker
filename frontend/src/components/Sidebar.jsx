557787470a106c180f6e0cd5b78faceb6ceacb2c
baohuy1303 <huynhbaohuy130333@gmail.com>
2025-06-21 17:02:50 +0700

    homepage buttons and navbar done, score comp done

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -18,7 +18,7 @@ function SideBar() {
 
     return (
         <>
-            <div className="fixed left-0 top-1/2 transform -translate-y-1/2 w-[6vw] bg-orange text-white p-6 shadow-lg/30 shadow-orange-400 rounded-r-xl space-y-6">
+            <div className="fixed left-0 top-1/2 transform -translate-y-1/2 w-[6vw] bg-orange text-white p-6 shadow-lg/30 shadow-orange-400 rounded-r-xl space-y-6 border-4 border-l-0 border-amber-50 hover:scale-105 hover:shadow-xl/25 transition duration-100 ease-in-out">
                 <div className="flex flex-col justify-center items-center gap-5 ">
                     <label className="cursor-pointer">
                         <input
@@ -28,7 +28,10 @@ function SideBar() {
                             onChange={handleChange}
                             className="peer hidden"
                         />
-                        <div className=" flex items-center justify-center size-[3vw] rounded-full border-4 border-black peer-checked-bg-greenSelect bg-bgColor shadow-orange-800 shadow-xl/30">
+                        <div
+                            className=" flex items-center justify-center size-[3vw] rounded-full border-4 border-black peer-checked-bg-greenSelect bg-bgColor shadow-orange-800 shadow-xl/30
+                            hover:bg-sidebar hover:border-sidebar hover:scale-110 transition duration-200 ease-in-out "
+                        >
                             <FontAwesomeIcon
                                 icon={fa1}
                                 className="text-black text-2xl"
@@ -44,7 +47,10 @@ function SideBar() {
                             onChange={handleChange}
                             className="peer hidden"
                         />
-                        <div className=" flex items-center justify-center size-[3vw] rounded-full border-4 border-black peer-checked-bg-greenSelect bg-bgColor  shadow-orange-800 shadow-xl/30">
+                        <div
+                            className=" flex items-center justify-center size-[3vw] rounded-full border-4 border-black peer-checked-bg-greenSelect bg-bgColor shadow-orange-800 shadow-xl/30
+                            hover:bg-sidebar hover:border-sidebar hover:scale-110 transition duration-200 ease-in-out "
+                        >
                             <FontAwesomeIcon
                                 icon={fa2}
                                 className="text-black text-2xl"
@@ -52,14 +58,14 @@ function SideBar() {
                         </div>
                     </label>
 
-                    <div className="flex items-center justify-center size-[3vw] rounded-full border-4 border-black bg-bgColor  shadow-orange-800 shadow-xl/30">
+                    <div className=" flex items-center justify-center size-[3vw] rounded-full border-4 border-black bg-bgColor shadow-orange-800 shadow-xl/30 hover:bg-sidebar hover:border-sidebar hover:scale-110 transition duration-200 ease-in-out cursor-pointer">
                         <FontAwesomeIcon
                             icon={faPen}
                             className="text-black text-2xl"
                         />
                     </div>
 
-                    <div className="flex items-center justify-center size-[3vw] rounded-full border-4 border-black bg-bgColor shadow-orange-800 shadow-xl/30">
+                    <div className=" flex items-center justify-center size-[3vw] rounded-full border-4 border-black bg-bgColor shadow-orange-800 shadow-xl/30 hover:bg-sidebar hover:border-sidebar hover:scale-110 transition duration-200 ease-in-out cursor-pointer">
                         <FontAwesomeIcon
                             icon={faRobot}
                             className="text-black text-2xl"
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,5 +1,8 @@
 import { Link, useNavigate, useLocation } from "react-router-dom"
 import '../css/navbar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRightFromBracket, faBullseye, faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 
 function Navbar(){
@@ -21,11 +24,22 @@ function Navbar(){
         }
     }
 
-    return<div className="navbar">
-        {!isHome && <button className="nav-item" onClick={handleBack}>Back</button>}
-    <Link className="nav-item" to={'/home'}>Home</Link>
-    <Link className="nav-item" to={'/add-subjects'}>Edit Subject List</Link>
+    return<div className="flex justify-center items-center gap-5">
+{!isHome &&
+<div className="mt-4 bg-amber-50 p-3 rounded-2xl shadow-xl/20 shadow-orange-400 transition duration-200 ease-in-out">
+ <button className="nav-item rounded-xl font-bold hover:bg-amber-100 hover:shadow-md/20 hover:scale-110 transition duration-200 ease-in-out" onClick={handleBack}><FontAwesomeIcon icon={faArrowLeft} className="mr-1"/> Back</button>
+</div>
+}
+    
+    
+    <div className="navbar mt-4 bg-amber-50 p-3 rounded-2xl shadow-xl/20 shadow-orange-400 transition duration-200 ease-in-out">
+    <a href="" className="nav-item rounded-xl font-bold hover:bg-amber-100 hover:shadow-md/20 hover:scale-110 transition duration-200 ease-in-out"><FontAwesomeIcon icon={faGithub} className="text-2xl"/></a>
+    <Link className="nav-item rounded-xl font-bold hover:bg-amber-100 hover:shadow-md/20 hover:scale-110 transition duration-200 ease-in-out" to={'/home'}><FontAwesomeIcon icon={faHouse}/> Home </Link>
+    <Link className="nav-item rounded-xl font-bold hover:bg-amber-100 hover:shadow-md/20 hover:scale-110 transition duration-200 ease-in-out" to={'/add-subjects'}><FontAwesomeIcon icon={faBullseye}/> Goals </Link>
+    <Link className="nav-item rounded-xl font-bold hover:bg-amber-100 hover:shadow-md/20 hover:scale-110 transition duration-200 ease-in-out" to={'/add-subjects'}> <FontAwesomeIcon icon={faArrowRightFromBracket}/> Log Out</Link>
     </div>
+       
+</div>
 }
 
 export default Navbar
