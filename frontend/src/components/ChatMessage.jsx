1d322aefb901c4cb9c2077ef1bb391a44b12a4e0
baohuy1303 <huynhbaohuy130333@gmail.com>
2025-06-23 22:27:21 +0700

    styling chatbot

--- frontend/src/components/ChatMessage.jsx
+++ frontend/src/components/ChatMessage.jsx
@@ -8,15 +8,18 @@ function ChatMessage({ chat }) {
                 <div className="flex flex-row justify-baseline">
                     <FontAwesomeIcon
                         icon={faRobot}
-                        className="text-black text-[1.2vw] self-end mr-2 bg-amber-100 rounded-[100%] px-2 py-2.5"
+                        className="text-black text-[1.2vw] self-end mr-2 bg-amber-100 rounded-[100%] px-2 py-2.5 
+                        shadow-md/20 border-3 border-bgColor"
                     />
-                    <p className=" text-[1vw] p-4 bg-gray-200 max-w-[75%] rounded-t-xl rounded-br-xl rounded-bl-0 break-words whitespace-pre-line">
+                    <p className=" text-[0.9vw] p-4 bg-gray-200 max-w-[75%] rounded-t-xl rounded-br-xl rounded-bl-0 break-words whitespace-pre-line
+                    shadow-md/20 border-3 border-gray-200">
                        {chat.text.data}
                     </p>
                 </div>
             ) : (
                 <div className="flex flex-row justify-end">
-                    <p className="text-[1vw] p-4 bg-amber-100 max-w-[75%] rounded-t-xl rounded-bl-xl rounded-br-0 break-words whitespace-pre-line">
+                    <p className="text-[1vw] p-4 bg-amber-100 max-w-[75%] rounded-t-xl rounded-bl-xl rounded-br-0 break-words whitespace-pre-line
+                    shadow-md/20 border-3 border-bgColor">
                         {chat.text}
                     </p>
                 </div>
--- frontend/src/pages/ChatBot.jsx
+++ frontend/src/pages/ChatBot.jsx
@@ -1,25 +1,37 @@
 import { faPaperPlane, faRobot } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ChatMessage from "../components/ChatMessage";
 import { chatBot } from "../api";
 import { useScoreContext } from "../contexts/ScoreContext";
 
 function ChatBot() {
     const inputRef = useRef()
+    const chatRef = useRef()
     const [chatHistory, setChatHistory] = useState([])
     const [loading, setLoading] = useState(false)
     const {score, subject, term} = useScoreContext()
 
+
+    useEffect(() => {
+        setChatHistory([])
+    }, [term])
+
         async function getBotRes(question) {
             setLoading(true);
+            setChatHistory((history) => [...history, {role: 'model', text: {data: 'Thinking...'}}]);
             try {
                 let data = await chatBot(question, score, chatHistory);
                 if (data) {
-                    setChatHistory((history) => [...history, {role: 'model', text: data}]);
+                    setChatHistory((history) => {
+                        const prevChats = [...history]
+                        prevChats[prevChats.length-1] = {role: 'model', text: data}
+                        return prevChats
+                    });
                 }
             } catch (error) {
-                console.error('Failed to fetch scores:', err);
+                console.error('Failed to fetch scores:', error);
+                setChatHistory((history) => [...history, { role: 'model', text: 'Sorry, something went wrong.' }]);
             } finally {
                 setLoading(false);
             }
@@ -32,9 +44,14 @@ function ChatBot() {
         inputRef.current.value = '';
 
         setChatHistory((history) => [...history, {role: 'user', text: userMessage}])
+
         
-            getBotRes(userMessage)
+        getBotRes(userMessage)
     }
+
+    useEffect(() => {
+        chatRef.current.scrollTo({top: chatRef.current.scrollHeight, behavior: 'smooth'})
+    }, [chatHistory])
 
     console.log(chatHistory)
 
@@ -49,13 +66,16 @@ function ChatBot() {
             </div>
             <p>Ask me anything related to your grades</p>
 
-            <div className=" flex flex-col bg-white w-[70vw] h-[60vh] rounded-xl p-5 overflow-auto gap-5 mt-5">
+            <div ref={chatRef} className=" flex flex-col bg-white w-[70vw] h-[60vh] rounded-xl p-5 overflow-auto gap-5 mt-5
+            shadow-md/20 shadow-orange-400 border-3 border-amber-100">
                 <div className="flex flex-row justify-baseline">
                     <FontAwesomeIcon
                         icon={faRobot}
-                        className="text-black text-[1.2vw] self-end mr-2 bg-amber-100 rounded-[100%] px-2 py-2.5"
+                        className="text-black text-[1.2vw] self-end mr-2 bg-amber-100 rounded-[100%] px-2 py-2.5 
+                        shadow-md/20 border-3 border-bgColor"
                     />
-                    <p className="text-[0.9vw] p-4 bg-gray-200 max-w-[75%] rounded-t-xl rounded-br-xl rounded-bl-0 break-words whitespace-pre-line">
+                    <p className="text-[0.9vw] p-4 bg-gray-200 max-w-[75%] rounded-t-xl rounded-br-xl rounded-bl-0 break-words whitespace-pre-line
+                    shadow-md/20 border-3 border-gray-200">
                         Hey there how can I help
                     </p>
                 </div>
@@ -81,9 +101,9 @@ function ChatBot() {
                             focus:outline-3 focus:outline-amber-200 shadow-md/30 focus:shadow-lg/30 shadow-orange-400 
                             hover:bg-amber-100 hover:shadow-lg/35  transition duration-200 ease-in-out"
                         />
-                        <button
-                            className="cursor-pointer bg-orange px-5 py-3 border-amber-50 rounded-lg shadow-lg/30 shadow-orange-400 
-                        hover:bg-sidebar hover:shadow-lg/35 hover:scale-110 transition duration-200 ease-in-out"
+                        <button disabled={loading}
+                            className={loading ? 'bg-gray-500 px-5 py-3 rounded-lg shadow-lg/30 shadow-orange-400' : `cursor-pointer bg-orange px-5 py-3 border-amber-50 rounded-lg shadow-lg/30 shadow-orange-400 
+                        hover:bg-sidebar hover:shadow-lg/35 hover:scale-110 transition duration-200 ease-in-out`}
                         >
                             <FontAwesomeIcon icon={faPaperPlane} />
                         </button>
