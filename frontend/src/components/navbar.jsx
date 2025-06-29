a74ad93de9ba2f38ef7ed6c4de8c3f1c8e7ac740
baohuy1303 <huynhbaohuy130333@gmail.com>
2025-06-29 23:24:50 +0700

    responsive css navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -30,21 +30,22 @@ function Navbar(){
     }
 
 
-    return<div className="flex justify-center items-center gap-5">
+    return<div className="flex flex-col-reverse sm:flex-row justify-center items-center">
 
         {!isHome &&
-        <div className="mt-4 bg-amber-50 p-3 rounded-2xl shadow-xl/20 shadow-orange-400 transition duration-200 ease-in-out">
-        <button className="nav-item rounded-xl font-bold hover:bg-amber-100 hover:shadow-md/20 hover:scale-110 transition duration-200 ease-in-out" onClick={handleBack}><FontAwesomeIcon icon={faArrowLeft} className="mr-1"/> Back</button>
+        <div className="self-start mx-5 mt-4 bg-amber-50 p-2 sm:p-3 lg:p-5 rounded-2xl shadow-xl/20 shadow-orange-400 transition duration-200 ease-in-out">
+        <button className="p-2.5 nav-item rounded-xl font-bold hover:bg-amber-100 hover:shadow-md/20 hover:scale-110 transition duration-200 ease-in-out" onClick={handleBack}><FontAwesomeIcon icon={faArrowLeft} className="mr-1"/> <span className="hidden sm:inline ml-0.5"> Back</span> </button>
         </div>
         }
     
     
-    <div className="navbar text-md mt-4 bg-amber-50 p-3 rounded-2xl shadow-xl/20 shadow-orange-400 transition duration-200 ease-in-out">
-    <a href="https://github.com/baohuy1303/score-aim-results-tracker" target="_blank" className="nav-item rounded-xl font-bold hover:bg-amber-100 hover:shadow-md/20 hover:scale-110 transition duration-200 ease-in-out"><FontAwesomeIcon icon={faGithub} className="text-2xl mt-0.5"/></a>
-    <Link className="nav-item rounded-xl font-bold hover:bg-amber-100 hover:shadow-md/20 hover:scale-110 transition duration-200 ease-in-out" to={'/home'}><FontAwesomeIcon icon={faHouse}/> Home </Link>
-    <Link className="nav-item rounded-xl font-bold hover:bg-amber-100 hover:shadow-md/20 hover:scale-110 transition duration-200 ease-in-out" to={'/add-subjects'}><FontAwesomeIcon icon={faPen}/> Edit Subjects </Link>
+    <div className="navbar w-[85%] sm:w-[70%] lg:w-[45%] lg:text-md mt-4 p-2 lg:p-4 text-sm sm:text-lg
+    bg-amber-50  rounded-2xl shadow-xl/20 shadow-orange-400 transition duration-200 ease-in-out">
+    <a href="https://github.com/baohuy1303/score-aim-results-tracker" target="_blank" className="p-2.5 nav-item rounded-xl font-bold hover:bg-amber-100 hover:shadow-md/20 hover:scale-110 transition duration-200 ease-in-out"><FontAwesomeIcon icon={faGithub} className="text-xl sm:text-2xl mt-0.5"/></a>
+    <Link className="p-2.5 nav-item rounded-xl font-bold hover:bg-amber-100 hover:shadow-md/20 hover:scale-110 transition duration-200 ease-in-out" to={'/home'}><FontAwesomeIcon icon={faHouse} className="mx-1"/> Home </Link>
+    <Link className="p-2.5 nav-item rounded-xl font-bold hover:bg-amber-100 hover:shadow-md/20 hover:scale-110 transition duration-200 ease-in-out" to={'/add-subjects'}><FontAwesomeIcon icon={faPen} className="mx-1"/> Edit<span className="hidden sm:inline ml-0.5"> Subjects</span> </Link>
 {/*     <Link className="nav-item rounded-xl font-bold hover:bg-amber-100 hover:shadow-md/20 hover:scale-110 transition duration-200 ease-in-out" to={'/goals'}><FontAwesomeIcon icon={faBullseye}/> Goals </Link> */}
-    <button onClick={handleLogOut} className="nav-item rounded-xl font-bold hover:bg-amber-100 hover:shadow-md/20 hover:scale-110 transition duration-200 ease-in-out"> <FontAwesomeIcon icon={faArrowRightFromBracket}/> Log Out</button>
+    <button onClick={handleLogOut} className="p-2.5 nav-item rounded-xl font-bold hover:bg-amber-100 hover:shadow-md/20 hover:scale-110 transition duration-200 ease-in-out"> <FontAwesomeIcon icon={faArrowRightFromBracket} className="mx-1"/>Log Out</button>
     </div>
        
 </div>
